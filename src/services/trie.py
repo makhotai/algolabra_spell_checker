0dfc32b69e7b776ee7e7e95815940d8cc0d8463f
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-09 15:10:10 +0300

    Fix insertion into Trie. Add node-search.

--- src/services/trie.py
+++ src/services/trie.py
@@ -2,20 +2,36 @@ class TrieNode:
     def __init__(self):
         self.word = None
         self.children = {}
+        self.is_word = False
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        self._size = 0
 
     def insert(self, word):
         if not word:
             raise ValueError("cannot insert an empty string into the trie")
 
         node = self.root
-        for letter in word:
-            if letter not in node.children:
-                node.children[letter] = TrieNode()
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
 
-            node = node.children[letter]
-
+        if not node.is_word:
+            self._size += 1
+        node.is_word = True
         node.word = word
+
+    def find_node(self, prefix: str):
+        node = self.root
+        for letter in prefix:
+            node = node.children.get(letter)
+            if node is None:
+                return None
+        return node
+
+    def contains(self, word: str):
+        node = self.find_node(word)
+        return node is not None and node.is_word
