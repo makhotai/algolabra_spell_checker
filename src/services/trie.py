337b90e461fc293f8597d1b664aa6b9ea5eb5717
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-16 19:46:03 +0300

    Add foreach loop for Trie

--- src/services/trie.py
+++ src/services/trie.py
@@ -55,6 +55,17 @@ class Trie:
                 return None
         return node
 
+    def list_generator(self, node: TrieNode):
+        words = []
+        if node.is_word:
+            words.append(node.word)
+        for ch in node.children: 
+            words.extend(self.list_generator(node.children[ch]))
+        return words
+
+    def words(self):
+        return self.list_generator(self.root)
+
     def contains(self, word: str):
         """checks if dictionary contains the word
 
