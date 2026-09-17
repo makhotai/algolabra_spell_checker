cae9b4211b96d9c59ba65dc708fd4a5211a867ec
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-17 11:54:53 +0300

    Implement add word to the vocabulary and check word spelling features

--- src/services/vocabulary.py
+++ src/services/vocabulary.py
@@ -1,12 +1,15 @@
-from trie import Trie
+from services.trie import Trie
+from services.osa import distance
 
 path_wl = "src/data/wordlist.txt"
 
-class Vocabulary:
+class SpellChecker:
     """uses Trie-data structure as a base to generate
     a vocabulary for spell-checker programm
     """
-    def __init__(self, trie=Trie(), path_wl=path_wl):
+    def __init__(self, trie=None, path_wl=path_wl):
+        if trie is None:
+            trie = Trie()
         self.trie = trie
         self.path_wl = path_wl
 
@@ -15,6 +18,15 @@ class Vocabulary:
         with open(self.path_wl, "r", encoding='UTF-8') as file:
             for row in file:
                 self.trie.insert(row.strip())
+
+    def add(self, word: str):
+        self.trie.insert(word.strip())
+
+    def is_correct(self, word: str):
+            return self.trie.contains(word)
+
+    def words(self):
+            return self.trie.words()
 
     def __len__(self):
         return len(self.trie)
