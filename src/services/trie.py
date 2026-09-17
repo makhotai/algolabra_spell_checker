0ad52aa72588cc96715b40173784a48db83a5d60
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-17 17:39:21 +0300

    More docstrings for Trie

--- src/services/trie.py
+++ src/services/trie.py
@@ -4,7 +4,7 @@ class TrieNode:
             word: complete word spelled by the path from the root
             children: maps a letter to the child ``TrieNode`` reached by that
             letter
-            is_word: shows if there is a such word
+            is_word: shows whether there is a word ending at this node
     """
     def __init__(self):
         self.word = None
@@ -24,9 +24,9 @@ class Trie:
             word (str): a word (as non-empty string) to insert
         """
         if not word:
-            return
+            return None
         if self.contains(word) is True:
-            return
+            return False
 
         node = self.root
         for char in word:
@@ -37,17 +37,10 @@ class Trie:
         self._size += 1
         node.is_word = True
         node.word = word
+        return True
 
     def find_node(self, prefix: str):
-        """searches 'prefix'/word in the trie
-
-        Args:
-            prefix (str): word to find
-
-        Returns:
-            bool: returns True if the word was previously inserted, 
-            otherwise returns False
-        """
+        """searches for a prefix or word in the trie"""
         node = self.root
         for letter in prefix:
             node = node.children.get(letter)
@@ -56,20 +49,30 @@ class Trie:
         return node
 
     def list_generator(self, node: TrieNode):
+        """returns a list of words in the subtree
+        rooted at the given node using dfs
+        """
         words = []
-        
+
         if node.is_word:
             words.append(node.word)
 
-        for ch in node.children: 
-            words.extend(self.list_generator(node.children[ch]))
+        for ch in node.children:
+            child_words = self.list_generator(node.children[ch])
+
+            for word in child_words:
+                words.append(word)
+
         return words
 
     def words(self):
-        return sorted(self.list_generator(self.root))
+        """returns list of all words contained in Trie"""
+        words = self.list_generator(self.root)
+        words.sort()
+        return words
 
     def contains(self, word: str):
-        """checks if dictionary contains the word
+        """checks whether the trie contains the given word
 
         Args:
             word (str): a word to find
@@ -82,5 +85,5 @@ class Trie:
         return node is not None and node.is_word
 
     def __len__(self):
-        """returns a number of words contained in the trie"""
+        """returns the number of words contained in the trie"""
         return self._size
