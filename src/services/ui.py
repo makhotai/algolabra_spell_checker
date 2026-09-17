989a563bad0dfa0cc66fa9ccad889bfa3832b65c
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-17 16:50:08 +0300

    Write UI menu

--- src/services/ui.py
+++ src/services/ui.py
@@ -6,10 +6,31 @@ class UI:
         self.sp.load_from_file()
     
     def start(self):
-        pass
+        self.menu()
+        option = input("(1-4): ")
+        if option == "1":
+            self.print_voc()
+
+        elif option == "2":
+            self.add_word()
+
+        elif option == "3":
+            self.check_word()
+            
+        elif option == "4":
+            self.quit()
+        
+        else:
+            print("invalid input, please try again")
+            print()
     
     def menu(self):
-        pass
+        print("choose one of the following options:")
+        print("1 - print vocabulary")
+        print("2 - add new word to vocabulary")
+        print("3 - check word spelling")
+        print("4 - quit")
+        print()
     
     def print_voc(self):
         pass
