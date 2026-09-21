d7d6c976630a3e470896383dd91069414f99c8f0
Taisiia Makhova <taisiia.makhova@helsinki.fi>
2026-09-21 18:14:21 +0300

    Test OSA distance with hypothesis

--- src/tests/osa_test.py
+++ src/tests/osa_test.py
@@ -1,3 +1,5 @@
+import hypothesis.strategies as st
+from hypothesis import given, settings
 import unittest
 from services.osa import distance
 
@@ -32,3 +34,20 @@ class TestOsa(unittest.TestCase):
         word2 = "zxcvbnm"
         n = max(len(word1), len(word2))
         self.assertEqual(distance(word1, word2), n)
+
+    @given(val=st.text(alphabet= st.characters(codec="utf-8"),
+                       min_size=2, max_size=70))
+    @settings(max_examples=500)
+    def test_distance_for_same_long_words_hypothesis(self, val):
+        self.assertEqual(distance(val, val), 0)
+    
+    @given(val1=st.text(alphabet= st.characters(codec="utf-8"), 
+                        min_size=3, max_size=40),
+           val2=st.text(alphabet= st.characters(codec="utf-8"),
+                        min_size=3, max_size=40))
+    @settings(max_examples=500)
+    def test_distance_for_longer_words_hypothesis(self, val1, val2):
+        if val1 != val2:
+            self.assertNotEqual(distance(val1, val2), 0)
+        else:
+            self.assertEqual(distance(val1, val2), 0)
